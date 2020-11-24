1f9b0bf566b41f328f8587c175250c4035873312
Sylvain Prat <sylvain.prat@gmail.com>
2020-11-24 21:43:46 +0100

    Add license, classifiers, keywords...

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ from setuptools import setup
 setup(
     name='sfrbox-client',
     description='SFR Box API Client',
+    license='MIT License',
+    keywords='sfr, box, api, client',
     use_scm_version=True,
     setup_requires=[
         'setuptools_scm',
@@ -24,5 +26,19 @@ setup(
             'pytest-pylint',
             'requests-mock'
         ],
-    }
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Environment :: MacOS X',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
 )
