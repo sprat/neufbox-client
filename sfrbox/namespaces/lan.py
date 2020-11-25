e23e4ab6b38b6cf7ef3aa5c969145163f4d78e62
Sylvain Prat <sylvain.prat@gmail.com>
2020-11-25 22:16:28 +0100

    Add the missing Lan methods

--- sfrbox/namespaces/lan.py
+++ sfrbox/namespaces/lan.py
@@ -1,7 +1,7 @@
 """
 Lan namespace module
 """
-from ._base import Namespace, GetMethod
+from ._base import Namespace, GetMethod, PostMethod
 
 
 class Lan(Namespace):
@@ -9,5 +9,8 @@ class Lan(Namespace):
     Lan API namespace definition
     """
     __namespace__ = 'lan'
-    get_info = GetMethod('getInfo')
+    add_dns_host = PostMethod('addDnsHost', params=['ip', 'name'])
+    delete_dns_host = PostMethod('deleteDnsHost', params=['ip', 'name'])
+    get_dns_host_list = GetMethod('getDnsHostList')
     get_hosts_list = GetMethod('getHostsList')
+    get_info = GetMethod('getInfo')
