666dc5e8a7aad4b9ecb4c139702f835d2895f945
Sylvain Prat <sylvain.prat@gmail.com>
2020-11-26 08:42:05 +0100

    Forgot a method in WLAN namespace

--- sfrbox/namespaces/wlan.py
+++ sfrbox/namespaces/wlan.py
@@ -19,6 +19,7 @@ class WLAN(Namespace):
     set_wl0_enc_type = PostMethod('setWl0Enctype', params=['enctype'])
     set_wl0_key_type = PostMethod('setWl0Keytype', params=['keytype'])
     set_wl0_ssid = PostMethod('setWl0Ssid', params=['ssid'])
+    set_wl0_wep_key = PostMethod('setWl0Wepkey', params=['wepkey'])
     set_wl0_wpa_key = PostMethod('setWl0Wpakey', params=['wpakey'])
     set_wlan_mode = PostMethod('setWlanMode', params=['mode'])
     start = PostMethod('start')
