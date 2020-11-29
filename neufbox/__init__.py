c898e9357535b4d6e60f4afea2e79c638956681e
Sylvain Prat <sylvain.prat@gmail.com>
2020-11-29 14:11:46 +0100

    Add version number in neufbox root module

--- neufbox/__init__.py
+++ neufbox/__init__.py
@@ -1,8 +1,10 @@
 """
 Neufbox Client root module
 """
+import pkg_resources
 from .client import Client, ClientError
 from .auth import username_password
 
 
 __all__ = ['Client', 'ClientError', 'username_password']
+__version__ = pkg_resources.get_distribution('neufbox-client').version
